1666704e1f2cd948ff0097cbcf11445bf0ae82c1
nervok1337 <124756097+nervok1337@users.noreply.github.com>
2025-05-26 17:03:24 +0800

    Комментарии Cell.cpp

--- C++/project/Cell.cpp
+++ C++/project/Cell.cpp
@@ -1,31 +1,38 @@
 #include "Cell.h"
 
+//Конструктор инициализирует закрытую ячейку без флага и с нулём мин вокруг
 Cell::Cell() {
 	isOpen = false;
 	isFlagged = false;
 	mineCount = 0;
 }
 
+//открывает ячейку
 void Cell::open() {
 	isOpen = true;
 }
+//ставит или убирает флаг
 void Cell::toggleFlag() {
 	isFlagged = !isFlagged;
 }
 
+//проверяет, является ли ячейка миной
 bool Cell::isMine() const {
 	return false;
 }
-
+//устанавливает число соседних мин 
 void Cell::setMines(int count) {
 	mineCount = count;
 }
+//возвращает количество мин вокруг ячейки
 int Cell::getMines() const {
 	return mineCount;
 }
+//проверяет, открыта ли ячейка
 bool Cell::getIsOpen() const {
 	return isOpen;
 }
+//проверяет, стоит ли флаг
 bool Cell::getIsFlagged() const {
 	return isFlagged;
 }
